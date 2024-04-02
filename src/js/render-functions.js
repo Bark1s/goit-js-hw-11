ef390d0d0747405df611a1c6d3d28eb963ee821c
Bark1s <sheda781997b@gmail.com>
2024-04-02 18:52:15 -0400

    no css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,5 +1,14 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+import { lightbox } from "../main";
+import {loadScreen} from "../main";
+import { hideLoadScreen } from "../main";
+
+export const gallery = document.querySelector(".gallery");
 
 export function renderImages(array) {
     if (array.length < 1) {
@@ -8,8 +17,46 @@ export function renderImages(array) {
             color: 'red',
             position: 'topCenter',
         });
+        hideLoadScreen();
     } else {
-        return console.log("fuck this");
+        const markup = array
+            .map((item) => {
+                return `<li class="item-box">
+                <a class="large-image" href="${item.largeImageURL}">
+                <img class="preview" src="${item.webformatURL}" alt="${item.tags}" />
+                </a>
+                <ul class="stat-list">
+                <li class="stat-item">
+                <p class="stat-value">
+                <span class="stat-name">Likes
+                </span>
+                ${item.likes}</p>
+                </li>
+                <li class="stat-item">
+                <p class="stat-value">
+                <span class="stat-name">Views
+                </span>
+                ${item.views}</p>
+                </li>
+                <li class="stat-item">
+                <p class="stat-value">
+                <span class="stat-name">Comments
+                </span>
+                ${item.comments}</p>
+                </li>
+                <li class="stat-item">
+                <p class="stat-value">
+                <span class="stat-name">Downloads
+                </span>
+                ${item.downloads}</p>
+                </li>
+                </ul>
+                </li>`;
+            })
+            .join("");
+        gallery.insertAdjacentHTML("beforeend", markup);
+        lightbox.refresh();
     }
+    hideLoadScreen();
 }
 
--- src/main.js
+++ src/main.js
@@ -1,25 +1,49 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { fetchImages } from "./js/pixabay-api";
-import { renderImages } from "./js/render-functions";
+import { gallery, renderImages } from "./js/render-functions";
+
+export const loadScreen = document.querySelector(".loader");
+
+function showLoadScreen() {
+    loadScreen.classList.remove("is-hidden")
+};
+
+export function hideLoadScreen() {
+    loadScreen.classList.add("is-hidden")
+};
 
 const form = document.querySelector(".form");
-const gallery = document.querySelector(".gallery");
+
+export const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    showLoadScreen();
+    gallery.innerHTML = "";
     let inputValue = event.target.elements.search.value.trim();
     if (inputValue !== "") {
         fetchImages(inputValue).then((resolve) => {
             renderImages(resolve.hits);
             form.reset();
         })
+            .catch((error) => {
+                console.log(error);
+                hideLoadScreen();
+            });
     } else {
         iziToast.show({
             message: 'Search field is empty',
             color: 'red',
             position: 'topCenter',
         });
+        hideLoadScreen();
     }
     });
